06ea8d3b53e0c77dfe54977968067558f0c53402
Josh Palmer <joshpalmer123@gmail.com>
2019-09-28 21:29:27 +0200

    Initial Commit

---
+++ WatchMetros WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchMetros WatchKit Extension
+//
+//  Created by Josh Palmer on 28/09/2019.
+//  Copyright © 2019 Josh Palmer. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchMetros WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WatchMetros WatchKit Extension
+//
+//  Created by Josh Palmer on 28/09/2019.
+//  Copyright © 2019 Josh Palmer. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
