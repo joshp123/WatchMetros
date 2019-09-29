749a6547e080dce6ae68c30efdeb0c3fc69b5928
Josh Palmer <joshpalmer123@gmail.com>
2019-09-29 12:30:29 +0200

    Added a complication with static data.

--- WatchMetros WatchKit Extension/ComplicationController.swift
+++ WatchMetros WatchKit Extension/ComplicationController.swift
@@ -33,7 +33,40 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
+        let url = URL(string: "http://localhost:5000/morning")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    print("statusCode: \(response.statusCode)")
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("data: \(dataString)")
+                }
+            }
+        }
+        task.resume()
+        
+        switch(complication.family) {
+        case .modularLarge:
+            let template = CLKComplicationTemplateModularLargeTable()
+            template.headerTextProvider = CLKSimpleTextProvider(text: "Metros from Blijdorp")
+            template.row1Column1TextProvider = CLKSimpleTextProvider(text: "S")
+            template.row1Column2TextProvider = CLKSimpleTextProvider(text: "5, 10, 15, every 5m")
+            template.row2Column1TextProvider = CLKSimpleTextProvider(text: "N")
+            template.row2Column2TextProvider = CLKSimpleTextProvider(text:
+                "5, 10, 15, every 15m")
+            // Create the timeline entry.
+           let entry = CLKComplicationTimelineEntry(date: Date(),
+                                                    complicationTemplate: template)
+           
+           // Pass the timeline entry to the handler.
+           handler(entry)
+
+        default:
+            handler(nil)
+        }
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -50,7 +83,24 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        
+        switch(complication.family) {
+        case .modularLarge:
+            let template = CLKComplicationTemplateModularLargeTable()
+            template.headerTextProvider = CLKSimpleTextProvider(text: "Metros from Fake")
+            template.row1Column1TextProvider = CLKSimpleTextProvider(text: "S")
+            template.row1Column2TextProvider = CLKSimpleTextProvider(text: "5, 10, 15, every 5m")
+            template.row2Column1TextProvider = CLKSimpleTextProvider(text: "N")
+            template.row2Column2TextProvider = CLKSimpleTextProvider(text:
+                "5, 10, 15, every 15m")
+           
+           // Pass the timeline entry to the handler.
+           handler(template)
+
+        default:
+            handler(nil)
+        }
+
     }
     
 }
